d262e1fc9870b6579248013c7c64f58758066aa9
sherry1004 <775757661@qq.com>
2020-02-16 08:23:02 +0800

    地图样式

--- src/components/mobile/chart/fk.vue
+++ src/components/mobile/chart/fk.vue
@@ -70,6 +70,7 @@ export default {
     blDataFix() {
       if (!this.blList.length) return;
       const today = this.$util.getTime();
+      console.log(today)
       const xqObj = {};
       this.ill_cure = [
         this.blList.length,
@@ -79,6 +80,7 @@ export default {
           ({ xzbq, cysj }) => xzbq == "出院" && cysj.includes(today)
         ).length
       ];
+      console.log(this.ill_cure)
       this.blList.map(({ xq, xzbq }) => {
         const _xq_ = xq.replace(/产业集聚区/g, "");
         !xqObj[_xq_] && (xqObj[_xq_] = [0, 0]);
